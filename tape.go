b466ffa91c9afdb22d20f5d23b004eef5582c7c0
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2024-05-26 13:00:18 +0200

    lfos and waveshapes on tape

--- tape.go
+++ tape.go
@@ -6,19 +6,19 @@ import (
 )
 
 type Tape struct {
-	channels int
-	length   int
-	samples  []Smp
+	nchannels int
+	nframes   int
+	samples   []Smp
 }
 
 func (t *Tape) String() string {
-	return fmt.Sprintf("Tape(channels=%d length=%d)", t.channels, t.length)
+	return fmt.Sprintf("Tape(nchannels=%d nframes=%d)", t.nchannels, t.nframes)
 }
 
 func (t *Tape) GetSampleIterator() SampleIterator {
 	sampleIndex := 0
-	end := t.length * t.channels
-	if t.channels == 1 {
+	end := t.nframes * t.nchannels
+	if t.nchannels == 1 {
 		return func() Smp {
 			if sampleIndex < end {
 				nextSample := t.samples[sampleIndex]
@@ -32,11 +32,11 @@ func (t *Tape) GetSampleIterator() SampleIterator {
 		return func() Smp {
 			if sampleIndex < end {
 				var nextSample Smp
-				for range t.channels {
+				for range t.nchannels {
 					nextSample += t.samples[sampleIndex]
 					sampleIndex++
 				}
-				return nextSample / float64(t.channels)
+				return nextSample / float64(t.nchannels)
 			} else {
 				return 0
 			}
@@ -44,33 +44,173 @@ func (t *Tape) GetSampleIterator() SampleIterator {
 	}
 }
 
+func clamp(value float64, lo float64, hi float64) float64 {
+	if value < lo {
+		return lo
+	}
+	if value > hi {
+		return hi
+	}
+	return value
+}
+
+func calcSin(phase float64) float64 {
+	return math.Sin(phase * 2 * math.Pi)
+}
+
+func calcPulse(phase, width float64) float64 {
+	if phase < width {
+		return -1.0
+	} else {
+		return 1.0
+	}
+}
+
+func calcTriangle(phase float64) float64 {
+	if phase < 0.25 {
+		return phase * 4.0
+	} else if phase < 0.75 {
+		return 1.0 - (phase-0.25)*4.0
+	} else {
+		return -1.0 + (phase-0.75)*4.0
+	}
+}
+
+func calcSaw(phase float64) float64 {
+	if phase < 0.5 {
+		return phase * 2.0
+	} else {
+		return -1.0 + (phase-0.5)*2.0
+	}
+}
+
 func (t *Tape) GetMessageHandler(msg string, nargs int) Fun {
 	if nargs == 0 {
 		switch msg {
-		case "sin":
+		case "pulse":
 			return func(vm *VM) error {
-				phase := vm.GetFloat(":phase")
-				incr := (2 * math.Pi) / float64(t.length)
-				for i := 0; i < t.length; {
-					smp := math.Sin(phase)
-					for range t.channels {
-						t.samples[i] = smp
-						i++
+				phase := clamp(vm.GetFloat(":phase"), 0, 1)
+				width := clamp(vm.GetFloat(":width"), 0, 1)
+				incr := 1.0 / float64(t.nframes)
+				writeIndex := 0
+				for i := 0; i < t.nframes; {
+					smp := calcPulse(phase, width)
+					for range t.nchannels {
+						t.samples[writeIndex] = smp
+						writeIndex++
 					}
 					phase += incr
 				}
 				return nil
 			}
-		case "osc.sin":
+		case "lfo.pulse":
 			return func(vm *VM) error {
 				sr := vm.GetFloat(":sr")
 				freq := GetSampleIterator(vm.GetVal(":freq"))
-				phase := vm.GetFloat(":phase")
-				for i := 0; i < t.length; {
-					smp := math.Sin(phase * 2 * math.Pi)
-					for range t.channels {
-						t.samples[i] = smp
-						i++
+				phase := clamp(vm.GetFloat(":phase"), 0, 1)
+				width := clamp(vm.GetFloat(":width"), 0, 1)
+				writeIndex := 0
+				for i := 0; i < t.nframes; {
+					smp := calcPulse(phase, width)
+					for range t.nchannels {
+						t.samples[writeIndex] = smp
+						writeIndex++
+					}
+					incr := 1.0 / (sr / freq())
+					phase = math.Mod(phase+incr, 1.0)
+				}
+				return nil
+			}
+		case "triangle":
+			return func(vm *VM) error {
+				phase := clamp(vm.GetFloat(":phase"), 0, 1)
+				incr := 1.0 / float64(t.nframes)
+				writeIndex := 0
+				for i := 0; i < t.nframes; {
+					smp := calcTriangle(phase)
+					for range t.nchannels {
+						t.samples[writeIndex] = smp
+						writeIndex++
+					}
+					phase += incr
+				}
+				return nil
+			}
+		case "lfo.triangle":
+			return func(vm *VM) error {
+				sr := vm.GetFloat(":sr")
+				freq := GetSampleIterator(vm.GetVal(":freq"))
+				phase := clamp(vm.GetFloat(":phase"), 0, 1)
+				writeIndex := 0
+				for i := 0; i < t.nframes; {
+					smp := calcTriangle(phase)
+					for range t.nchannels {
+						t.samples[writeIndex] = smp
+						writeIndex++
+					}
+					incr := 1.0 / (sr / freq())
+					phase = math.Mod(phase+incr, 1.0)
+				}
+				return nil
+			}
+		case "saw":
+			return func(vm *VM) error {
+				phase := clamp(vm.GetFloat(":phase"), 0, 1)
+				incr := 1.0 / float64(t.nframes)
+				writeIndex := 0
+				for i := 0; i < t.nframes; {
+					smp := calcSaw(phase)
+					for range t.nchannels {
+						t.samples[writeIndex] = smp
+						writeIndex++
+					}
+					phase += incr
+				}
+				return nil
+			}
+		case "lfo.saw":
+			return func(vm *VM) error {
+				sr := vm.GetFloat(":sr")
+				freq := GetSampleIterator(vm.GetVal(":freq"))
+				phase := clamp(vm.GetFloat(":phase"), 0, 1)
+				writeIndex := 0
+				for i := 0; i < t.nframes; {
+					smp := calcSaw(phase)
+					for range t.nchannels {
+						t.samples[writeIndex] = smp
+						writeIndex++
+					}
+					incr := 1.0 / (sr / freq())
+					phase = math.Mod(phase+incr, 1.0)
+				}
+				return nil
+			}
+		case "sin":
+			return func(vm *VM) error {
+				phase := clamp(vm.GetFloat(":phase"), 0, 1)
+				incr := 1.0 / float64(t.nframes)
+				writeIndex := 0
+				for i := 0; i < t.nframes; {
+					smp := calcSin(phase)
+					for range t.nchannels {
+						t.samples[writeIndex] = smp
+						writeIndex++
+					}
+					phase += incr
+				}
+				return nil
+			}
+		case "lfo.sin":
+			return func(vm *VM) error {
+				sr := vm.GetFloat(":sr")
+				freq := GetSampleIterator(vm.GetVal(":freq"))
+				phase := clamp(vm.GetFloat(":phase"), 0, 1)
+				writeIndex := 0
+				for i := 0; i < t.nframes; {
+					smp := calcSin(phase)
+					for range t.nchannels {
+						t.samples[writeIndex] = smp
+						writeIndex++
 					}
 					incr := 1.0 / (sr / freq())
 					phase = math.Mod(phase+incr, 1.0)
@@ -82,23 +222,25 @@ func (t *Tape) GetMessageHandler(msg string, nargs int) Fun {
 	return nil
 }
 
-func pushTape(vm *VM, channels, length int) {
-	samples := make([]Smp, channels*length)
+func pushTape(vm *VM, nchannels, nframes int) {
+	samples := make([]Smp, nchannels*nframes)
 	tape := &Tape{
-		channels: channels,
-		length:   length,
-		samples:  samples,
+		nchannels: nchannels,
+		nframes:   nframes,
+		samples:   samples,
 	}
 	vm.PushVal(tape)
 }
 
 func wordTape1(vm *VM) error {
-	pushTape(vm, 1, vm.GetInt(":length"))
+	nframes := int(vm.PopNum())
+	pushTape(vm, 1, nframes)
 	return nil
 }
 
 func wordTape2(vm *VM) error {
-	pushTape(vm, 2, vm.GetInt(":length"))
+	nframes := int(vm.PopNum())
+	pushTape(vm, 2, nframes)
 	return nil
 }
 
