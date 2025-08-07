17d3a9dae7b7bc98a26f50499220559ea908e85a
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2025-08-07 21:49:47 +0200

    envelope segments

--- env.go
+++ env.go
@@ -1,17 +1,87 @@
 package main
 
+import (
+	"math"
+)
+
+func envseg(start, end float64, nframes int, shape func(float64) float64) *Tape {
+	t := makeTape(1, nframes)
+	var x float64
+	incr := 1.0 / float64(nframes)
+	for i := range nframes {
+		t.samples[i] = start + float64(end-start)*shape(x)
+		x += incr
+	}
+	return t
+}
+
 func init() {
-	RegisterWord("line", func(vm *VM) error {
-		end := Smp(Pop[Num](vm))
-		nf := int(Pop[Num](vm))
-		curr := Smp(Pop[Num](vm))
-		incr := (end - curr) / Smp(nf)
-		t := pushTape(vm, 1, nf)
-		for i := range nf {
-			t.samples[i] = curr
-			curr += incr
-		}
-		vm.Push(end)
+	RegisterWord("line/", func(vm *VM) error {
+		start := float64(Get[Num](vm, ":start"))
+		end := float64(Get[Num](vm, ":end"))
+		nf := int(Get[Num](vm, ":nf"))
+		vm.Push(envseg(start, end, nf, func(x float64) float64 { return x }))
 		return nil
 	})
+
+	RegisterWord("exp/", func(vm *VM) error {
+		k := float64(Pop[Num](vm))
+		start := float64(Get[Num](vm, ":start"))
+		end := float64(Get[Num](vm, ":end"))
+		nf := int(Get[Num](vm, ":nf"))
+		if k == 0 {
+			vm.Push(envseg(start, end, nf, func(x float64) float64 { return x }))
+		} else {
+			vm.Push(envseg(start, end, nf, func(x float64) float64 {
+				return (math.Exp(k*x) - 1) / (math.Exp(k) - 1)
+			}))
+		}
+		return nil
+	})
+
+	RegisterWord("log/", func(vm *VM) error {
+		k := float64(Pop[Num](vm))
+		start := float64(Get[Num](vm, ":start"))
+		end := float64(Get[Num](vm, ":end"))
+		nf := int(Get[Num](vm, ":nf"))
+		if k == 0 {
+			vm.Push(envseg(start, end, nf, func(x float64) float64 { return x }))
+		} else {
+			vm.Push(envseg(start, end, nf, func(x float64) float64 {
+				return math.Log(1+k*x) / math.Log(1+k)
+			}))
+		}
+		return nil
+	})
+
+	RegisterWord("cos/", func(vm *VM) error {
+		start := float64(Get[Num](vm, ":start"))
+		end := float64(Get[Num](vm, ":end"))
+		nf := int(Get[Num](vm, ":nf"))
+		vm.Push(envseg(start, end, nf, func(x float64) float64 {
+			return 0.5 - 0.5*math.Cos(math.Pi*x)
+		}))
+		return nil
+	})
+
+	RegisterWord("pow/", func(vm *VM) error {
+		p := float64(Pop[Num](vm))
+		start := float64(Get[Num](vm, ":start"))
+		end := float64(Get[Num](vm, ":end"))
+		nf := int(Get[Num](vm, ":nf"))
+		vm.Push(envseg(start, end, nf, func(x float64) float64 { return math.Pow(x, p) }))
+		return nil
+	})
+
+	RegisterWord("sigmoid/", func(vm *VM) error {
+		k := float64(Pop[Num](vm))
+		start := float64(Get[Num](vm, ":start"))
+		end := float64(Get[Num](vm, ":end"))
+		nf := int(Get[Num](vm, ":nf"))
+		vm.Push(envseg(start, end, nf, func(x float64) float64 {
+			return 1 / (1 + math.Exp(-k*(x-0.5)))
+		}))
+		return nil
+	})
+
 }
