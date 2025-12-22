189a6fed7ecc2f2b496b5a04968c26894c7fd49d
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2025-12-22 11:41:27 +0100

    move DC blockers to dc.go

--- streams.go
+++ streams.go
@@ -233,34 +233,6 @@ func (s Stream) WithNChannels(nchannels int) Stream {
 	return s
 }
 
-// DCBlock applies a simple one-pole high-pass filter to remove DC offset.
-// alpha controls the cutoff; typical small value like 0.995.
-func DCBlock(s Stream, alpha float64) Stream {
-	return makeTransformStream([]Stream{s}, func(yield func(Frame) bool) {
-		out := make(Frame, s.nchannels)
-		prevIn := make([]Smp, s.nchannels)
-		prevOut := make([]Smp, s.nchannels)
-		for frame := range s.seq {
-			for c := range s.nchannels {
-				y := frame[c] - prevIn[c] + Smp(alpha)*prevOut[c]
-				prevIn[c] = frame[c]
-				prevOut[c] = y
-				out[c] = y
-			}
-			if !yield(out) {
-				return
-			}
-		}
-	})
-}
-
-// DCFilter implements Vital's dc_filter: y[n] = (x[n]-x[n-1]) + a*y[n-1]
-// with a = 1 - (1 / sampleRate). It's a very low cutoff (~0.16 Hz @ 48kHz).
-func DCFilter(s Stream) Stream {
-	alpha := 1.0 - 1.0/float64(SampleRate())
-	return DCBlock(s, alpha)
-}
-
 func (s Stream) Combine(other Stream, op SmpBinOp) Stream {
 	nchannels := s.nchannels
 	return makeTransformStream([]Stream{s, other}, func(yield func(Frame) bool) {
@@ -384,32 +356,6 @@ func init() {
 			return err
 		}
 		vm.Push(Phasor(freq, phase, PulseOp(pw)))
-		return nil
-	})
-
-	RegisterWord("dcblock", func(vm *VM) error {
-		stream, err := streamFromVal(vm.Pop())
-		if err != nil {
-			return err
-		}
-		alpha := 0.995
-		if aval := vm.GetVal(":alpha"); aval != nil {
-			if anum, ok := aval.(Num); ok {
-				alpha = float64(anum)
-			} else {
-				return fmt.Errorf("dcblock: :alpha must be number")
-			}
-		}
-		vm.Push(DCBlock(stream, alpha))
-		return nil
-	})
-
-	RegisterWord("dcfilter", func(vm *VM) error {
-		stream, err := streamFromVal(vm.Pop())
-		if err != nil {
-			return err
-		}
-		vm.Push(DCFilter(stream))
 		return nil
 	})
 
